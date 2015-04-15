7dd4932f04eebaa6248e6e6ff557277afe14e042
Rob Monk <robert.monk85@gmail.com>
2015-04-15 10:31:45 -0400

    fix deployment issues

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -8,6 +8,11 @@ FactoryGirl.define do
     sequence(:category) { |n| "category#{n}" }
   end
 
+  factory :user do
+    sequence(:email) { |n| "useragain#{n}@mymail.com" }
+    password "supersecretstuffhere"
+  end
+
   factory :event do
     sequence(:title) { |n| "My awesome event #{n}" }
     details "Super cool fun rad totally jivin event of the decade"
@@ -17,10 +22,5 @@ FactoryGirl.define do
     city "Boston"
     state "Massachusetts"
     zip_code "02134"
-  end
-
-  factory :user do
-    sequence(:email) { |n| "useragain#{n}@mymail.com" }
-    password "supersecretstuffhere"
   end
 end
