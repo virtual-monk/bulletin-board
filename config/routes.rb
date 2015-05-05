5e3d760ec8d616a6984824f24cbd8f4b1e25bf8e
Rob Monk <robert.monk85@gmail.com>
2015-05-05 16:26:55 -0400

    Fix routes for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: "events#index"
   get '/map', to:'events#map'
   get '/profile', to:'events#profile'
-  resources :profile
   resource :calendar, only: [:show], controller: :calendar
   devise_for :users
   resources :map
